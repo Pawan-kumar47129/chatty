6f986a0feb82328fc477d3c871d092906d33e0a2
Pawan-kumar47129 <pawan937281@gmail.com>
2025-06-22 19:21:45 +0530

    set up database for production

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,13 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB=async()=>{
-    try{
-    const conn=await mongoose.connect(process.env.MONGODB_URL);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      connectTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
-    }
-    catch(err){
-        console.log(err);
-        process.exit(1);
-    }
-}
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 export default connectDB;
